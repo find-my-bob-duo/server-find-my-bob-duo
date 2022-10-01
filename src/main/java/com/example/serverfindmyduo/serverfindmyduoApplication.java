fe8349b56079533078ee3bc27e683c70c25c59b0
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-10-01 22:00:33 +0900

    gradle 파일

---
+++ src/main/java/com/example/serverfindmyduo/serverfindmyduoApplication.java
@@ -0,0 +1,10 @@
+package com.example.serverfindmyduo;
+
+import org.springframework.boot.SpringApplication;
+
+public class serverfindmyduoApplication {
+
+    public static void main (String[] args){
+        SpringApplication.run(serverfindmyduoApplication.class, args);
+    }
+}
